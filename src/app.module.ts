28df29dadb50a571392ffa966de2e55e8bbb9815
valentinesamuel <valentinesamuel2580@gmail.com>
2025-04-06 15:32:12 +0100

    chore: added otel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-import "./shared/observability/tracing";
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { ConfigModule, ConfigService } from "@nestjs/config";
